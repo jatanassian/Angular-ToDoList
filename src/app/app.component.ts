d8b75b136f38582365e7c6ef3ff152fe46cdbceb
Julien Atanassian <julien.atanassian@gmail.com>
2020-01-22 16:59:03 -0500

    clean the component from the tinkering code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name:string = "Julien";
 
-  constructor() {
-    this.changeName('Ted');
-  }
-
-  // :void means it doesn't return anything
-  changeName(name:string):void {
-    this.name = name;
-  }
 }
