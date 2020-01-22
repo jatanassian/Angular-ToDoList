36c299ca70a588c78e701d7c816b8f285b426264
Julien Atanassian <julien.atanassian@gmail.com>
2020-01-22 17:18:37 -0500

    add comments to explain details

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -7,7 +7,7 @@ import { Todo } from '../../models/Todo';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-  // The [] specify that this is gonna be an array.
+  // The [] specify that this is gonna be an array. The app doesn't know what a Todo is so we created a Todo model in the models folder that we imported.
   todos:Todo[];
 
   constructor() { }
--- src/app/models/Todo.ts
+++ src/app/models/Todo.ts
@@ -1,4 +1,5 @@
 export class Todo {
+  // In case a field is optional we can add a question mark like 'id?:number;'
   id:number;
   title:string;
   completed:boolean;
