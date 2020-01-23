9a3f2665d2199358a26fe666aa6ec4998035b6d4
Julien Atanassian <julien.atanassian@gmail.com>
2020-01-23 18:13:58 -0500

    add a second property to the class which will limit the number of todos we're getting from the api and add that new property to the url of the get request

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -11,12 +11,14 @@ export class TodoService {
   // Add the url as a property of this class
   todosUrl:string = 'https://jsonplaceholder.typicode.com/todos';
 
+  todosLimit:string = '?_limit=5';
+
   constructor(private http:HttpClient) { }
 
   // Method that makes the get request just like axios or the fetch api.
   // With '<Todo[]>' we're specifying that it's gonna be a Todo type and an array.
   // The Observable is an asynchronous data flow. Like a  promise. On the client we'll then subscribe to that Observable which is like a '.then' in js.
   getTodos():Observable<Todo[]> {
-    return this.http.get<Todo[]>(this.todosUrl);
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
 }
