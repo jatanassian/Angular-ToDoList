63736fa1126403d427f17174011706a4605f08e2
Julien Atanassian <julien.atanassian@gmail.com>
2020-01-23 17:45:18 -0500

    move the todos from the component to the service

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -13,24 +13,7 @@ export class TodosComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.todos = [
-      {
-        // With typescript, the app pays attention to the field and its type as well. If we change 'false' from completed by '1' it will show an error.
-        id: 1,
-        title: 'Todo One',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Todo Two',
-        completed: true
-      },
-      {
-        id: 3,
-        title: 'Todo Three',
-        completed: false
-      }
-    ];
+
   }
 
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -6,4 +6,25 @@ import { Injectable } from '@angular/core';
 export class TodoService {
 
   constructor() { }
+
+  getTodos() {
+    return [
+      {
+        // With typescript, the app pays attention to the field and its type as well. If we change 'false' from completed by '1' it will show an error.
+        id: 1,
+        title: 'Todo One',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Todo Two',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'Todo Three',
+        completed: false
+      }
+    ];
+  }
 }
