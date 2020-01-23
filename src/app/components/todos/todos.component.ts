c66cb45a87838b3990daf76c699afa53ffa916ec
Julien Atanassian <julien.atanassian@gmail.com>
2020-01-23 17:49:32 -0500

    add comment

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -11,6 +11,7 @@ export class TodosComponent implements OnInit {
   // The [] specify that this is gonna be an array. The app doesn't know what a Todo is so we created a Todo model in the models folder that we imported.
   todos:Todo[];
 
+  // 'Private' means it only works here
   constructor(private todoService:TodoService) { }
 
   ngOnInit() {
