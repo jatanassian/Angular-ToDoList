0c377e59cdfbb158af47a6e0e40ff06007de11ed
Julien Atanassian <julien.atanassian@gmail.com>
2020-01-29 17:23:27 -0500

    construct the new todo and emit it upwards with an Output

--- src/app/components/add-todo/add-todo.component.ts
+++ src/app/components/add-todo/add-todo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-add-todo',
@@ -6,11 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-todo.component.css']
 })
 export class AddTodoComponent implements OnInit {
+  @Output() addTodo: EventEmitter<any> = new EventEmitter(); // We're using <any> because it doesn't have an id, it's not formatted as our model
+
   title:string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    const todo = {
+      // We don't need the id because jsonplaceholder creates the id for us
+      title: this.title,
+      completed: false
+    }
+
+    // Just like delete, we have to emit this to the parent because we need access to the direct todos in the todo component
+    this.addTodo.emit(todo);
   }
 
 }
